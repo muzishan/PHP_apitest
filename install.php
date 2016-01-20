71d302a107ec6dde1df8158a61efb92902c34b67
kantai <18722516087@163.com>
2016-01-20 15:39:53 +0800

    安装文件

---
+++ install.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kantai
+ * Date: 16/1/20
+ * Time: 下午3:23
+ */
+echo "<html>";
+echo "<head>";
+echo "<meta charset=\"utf-8\">";
+echo "<title>安装程序</title>";
+echo "</head>";
+echo "<body>";
+echo "</body>";
+echo "</html>";
